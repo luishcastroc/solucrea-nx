27083890878dbb0ee0225f0b0480a6c06c06af4d
Luis Castro <luish.castroc@gmail.com>
2021-12-13 08:03:19 -0500

    some functionality to creditos

--- apps/solucrea/src/app/modules/creditos/creditos-detail/creditos-detail.component.ts
+++ apps/solucrea/src/app/modules/creditos/creditos-detail/creditos-detail.component.ts
@@ -7,6 +7,8 @@ import { Observable } from 'rxjs';
 import { GetCreditosConfiguration } from '../_store/creditos.actions';
 import { CreditosState } from '../_store/creditos.state';
 import { Producto } from '.prisma/client';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
     selector: 'app-creditos-detail',
@@ -17,15 +19,22 @@ import { Producto } from '.prisma/client';
 export class CreditosDetailComponent implements OnInit {
     @Select(CreditosState.productos) productos$: Observable<Producto[]>;
     @Select(CreditosState.loading) loading$: Observable<boolean>;
+    clienteId: string;
+    creditoId: string;
+    creditosForm: FormGroup;
     constructor(
         private _store: Store,
         private _actions$: Actions,
         private _toast: HotToastService,
-        private location: Location
+        private location: Location,
+        private _route: ActivatedRoute,
+        private _formBuilder: FormBuilder
     ) {}
 
     ngOnInit(): void {
         this._store.dispatch(new GetCreditosConfiguration());
+        this.clienteId = this._route.snapshot.paramMap.get('clienteId');
+        this.creditoId = this._route.snapshot.paramMap.get('creditoId');
     }
 
     /**
@@ -33,5 +42,28 @@ export class CreditosDetailComponent implements OnInit {
      */
     back(): void {
         this.location.back();
+    }
+
+    /**
+     * Initialize Creditos
+     *
+     * @param clienteId
+     * @param creditoId
+     */
+    initCredito(clienteId: string, creditoId: string): void {}
+
+    /**
+     * Create Creditos Form
+     *
+     */
+    createCreditosForm(): FormGroup {
+        return this._formBuilder.group({
+            id: [''],
+            fechaInicio: ['', Validators.required],
+            antiguedad: [null, Validators.required],
+            actividadEconomica: ['', Validators.required],
+            montoMinimo: [null, Validators.required],
+            montoMaximo: [null, Validators.required],
+        });
     }
 }
---
+++ prisma/migrations/20211213130309_credito_modify/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `Creditos` MODIFY `fechaLiquidacion` DATETIME(3) NULL;
